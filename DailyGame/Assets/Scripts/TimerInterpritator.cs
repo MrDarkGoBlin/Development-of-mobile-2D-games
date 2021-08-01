9bc8eae08c4bd25f61cf494d3b204624d2889382
RaMy <vanya.kozlov.1998@mail.ru>
2021-08-01 19:27:55 +0300

    HW6
    Сделал ежедневные награды(еженедельки не делал), кое как сделал сохранение прогресса, и сделал бар (следующая ячейка заполняется по мере приближения срока награды)

---
+++ DailyGame/Assets/Scripts/TimerInterpritator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public static class TimerInterpritator 
+{
+    public static string InterpritationTime(float time)
+    {
+        string interptitationTime = "Time: ";
+
+        if (time / 3600 >= 1)
+        {
+            float hours = time / 3600;
+            hours = Mathf.Round(hours);
+            interptitationTime += $"{hours}h";
+        }
+        else if (time / 60 >= 1)
+        {
+            float minuts = time / 60;
+            minuts = Mathf.Round(minuts);
+            interptitationTime += $"{minuts}m";
+        }
+        else
+        {
+            time = Mathf.Round(time);
+            interptitationTime += $"{time}s";
+        }
+
+
+        return interptitationTime;
+    }
+}
