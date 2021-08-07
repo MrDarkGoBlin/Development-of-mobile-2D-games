9aeba623ea4ebf192625091198ad2ee85a2caf02
RaMy <vanya.kozlov.1998@mail.ru>
2021-08-07 19:35:04 +0300

    HW7
    немного поменял код "исправив" ошибку со сменой позиции вращения кнопки во время множественного нажатья кнопки, пытался собрать слайдер, но во первых не нашел где искать такие названия как m_Interactable. В общем он не работает.

---
+++ TweenGame/Assets/Scripts/CustomButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomButton : Button
+{
+    public static string ChangeButtonType => nameof(_animationButtonType);
+    public static string CurveEase => nameof(_curveEase);
+    public static string Duration => nameof(_duration);
+
+    [SerializeField]
+    private AnimationButtonType _animationButtonType = AnimationButtonType.ChangePosition;
+
+    [SerializeField]
+    private Ease _curveEase = Ease.Linear;
+
+    [SerializeField]
+    private float _duration = 0.6f;
+
+    private float _strength = 30.0f;
+    private RectTransform _rectTransform;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public override void OnPointerClick(PointerEventData eventData) 
+    {
+        base.OnPointerClick(eventData);
+        ActivateAnimation();
+    }
+
+    private void ActivateAnimation()
+    {
+        switch (_animationButtonType)
+        {
+            case AnimationButtonType.ChangeRotation:
+                var sequence = DOTween.Sequence();
+                sequence.Insert(0.0f, _rectTransform.DOShakeRotation(_duration, Vector3.forward * _strength).SetEase(_curveEase));
+                sequence.OnComplete(() => 
+                {
+                    //необходимо, так как при множественном клике кнопка изменяет свое стандартное положение(в параметре rotation)
+                    _rectTransform.DORotate(Vector3.zero, 0.1f); 
+                });
+                break;
+            case AnimationButtonType.ChangePosition:
+                _rectTransform.DOShakeAnchorPos(_duration, Vector2.one * _strength).SetEase(_curveEase);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ TweenGame/Assets/Scripts/Editor/CustomSliderEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(CustomSlider))]
+public class CustomSliderEditor : SliderEditor
+{
+    private SerializedProperty m_InteractableProperty;
+
+    protected override void OnEnable()
+    {
+        m_InteractableProperty = serializedObject.FindProperty("m_Interactable");
+    }
+
+    public override VisualElement CreateInspectorGUI()
+    {
+        var root = new VisualElement();
+
+        var colorOnClick = new PropertyField(serializedObject.FindProperty(CustomSlider.ColorOnClick));
+        var imageBur = new PropertyField(serializedObject.FindProperty(CustomSlider.ImageBur));
+        var duration = new PropertyField(serializedObject.FindProperty(CustomSlider.Duration));
+
+        var tweenLabel = new Label("Settings Tween");
+        var intractableLabel = new Label("Intractable");
+
+        root.Add(tweenLabel);
+        root.Add(colorOnClick);
+        root.Add(imageBur);
+        root.Add(duration);
+
+        root.Add(intractableLabel);
+        root.Add(new IMGUIContainer(OnInspectorGUI));
+
+        return root;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(m_InteractableProperty);
+
+        EditorGUI.BeginChangeCheck();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
